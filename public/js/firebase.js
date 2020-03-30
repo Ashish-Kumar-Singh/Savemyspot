87b2f7a4391b5f91d93c528826406292cbe5a75e
Ashish Kumar Singh <blr.ashu@gmail.com>
2020-03-30 18:06:05 +0100

    changes

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,23 +1,23 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.13.1/firebase-app.js"></script>
 
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.13.1/firebase-analytics.js"></script>
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyCvBERMdzRZe-mJKO3Qch2fsqX_ea-2EKM",
+  //   authDomain: "savemy-spot.firebaseapp.com",
+  //   databaseURL: "https://savemy-spot.firebaseio.com",
+  //   projectId: "savemy-spot",
+  //   storageBucket: "savemy-spot.appspot.com",
+  //   messagingSenderId: "992890459850",
+  //   appId: "1:992890459850:web:8ee35a0e8cb7c728718633",
+  //   measurementId: "G-43CWYBLZS6"
+  // };
+  // firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+  //Get Elements
+  const user_email = document.getElementById('email');
+  const user_password = document.getElementById('password');
 
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCvBERMdzRZe-mJKO3Qch2fsqX_ea-2EKM",
-    authDomain: "savemy-spot.firebaseapp.com",
-    databaseURL: "https://savemy-spot.firebaseio.com",
-    projectId: "savemy-spot",
-    storageBucket: "savemy-spot.appspot.com",
-    messagingSenderId: "992890459850",
-    appId: "1:992890459850:web:8ee35a0e8cb7c728718633",
-    measurementId: "G-43CWYBLZS6"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
+      const email = user_email.value;
+      const pass  = user_password.value;
+
+
+      const create  = auth.createUserWithEmailAndPassword(email, pass);
+      create.catch( e => console.log(e.message));
--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,16 @@
+firebase.auth().onAuthStateChanged(user => {
+  if(user) {
+    window.location = 'home.html'; //After successful login, user will be redirected to home.html
+  }
+});
+function signup(){
+  var user_email = document.getElementById('email').value;
+  var user_password = document.getElementById('password').value;
 
-
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCvBERMdzRZe-mJKO3Qch2fsqX_ea-2EKM",
-    authDomain: "savemy-spot.firebaseapp.com",
-    databaseURL: "https://savemy-spot.firebaseio.com",
-    projectId: "savemy-spot",
-    storageBucket: "savemy-spot.appspot.com",
-    messagingSenderId: "992890459850",
-    appId: "1:992890459850:web:8ee35a0e8cb7c728718633",
-    measurementId: "G-43CWYBLZS6"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-
-
-//Get elements
-const useremail = document.getElementById('email');
-const userpassword = document.getElementById('password');
-const btnlogin = document.getElementById('Login');
-
-btnlogin.addEventListener('click',e => {
-  const email = useremail.value;
-  const pass  = userpassword.value;
-  const auth = firebase.auth();
-
-  //Sign in
-  const promise  = auth.signInWithEmailAndPassword(email,pass);
-  promise.catch(e => console.log(e.message));
-})
+  firebase.auth().signInWithEmailAndPassword(user_email, user_password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}
