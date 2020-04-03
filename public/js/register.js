4001bb568e5f67d3d58cbfed929733f9da7747cc
Ashish Kumar singh <blr.ashu@gmail.com>
2020-04-03 15:11:06 +0100

    Added frontend check

--- public/js/register.js
+++ public/js/register.js
@@ -6,7 +6,32 @@ auth.onAuthStateChanged(user => {
         // window.location = 'index.html';
     }
   });
-
+  function checkBreachedPassword() {
+    var password = document.getElementById("signup-password-1").value;
+    var passwordDigest = new Hashes.SHA1().hex(password);
+    var digestFive = passwordDigest.substring(0, 5).toUpperCase();
+    var queryURL = "https://api.pwnedpasswords.com/range/" + digestFive;
+    var checkDigest = passwordDigest.substring(5, 41).toUpperCase();
+    var result;
+  
+  $.ajax({
+      url: queryURL,
+      type: 'GET',
+      async: false,
+      success: function(res) {
+        if (res.search(checkDigest) > -1){
+          result = false;
+          strength = false;
+          document.getElementById("result").innerHTML = "Choose a stronger password"
+        } else {
+          result = true;
+          strength = true;
+          document.getElementById("result").innerHTML = ""
+        }
+      }
+    });
+    return result;
+  }
   try{
     const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -14,18 +39,25 @@ signupForm.addEventListener('submit', (e) => {
   
   // get user info
   const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password-1'].value;
-try{
-    // sign up the user
-    auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      console.log(error.Message);
-    });
-}
-catch(err){
-  console.log(err);
-}
+  const password_1 = signupForm['signup-password-1'].value;
+  const password_2 = signupForm['signup-password-2'].value;
+  if(password_1 == password_2){
+    try{
+      // sign up the user
+      auth.createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(error.Message);
+      });
+  }
+  catch(err){
+    console.log(err);
+    document.getElementById("result").innerHTML = "Unable to register"
+  }
+  }
+  else{
+    document.getElementById("result").innerHTML = "Passwords do not match"
+  }
 
 });
   }
